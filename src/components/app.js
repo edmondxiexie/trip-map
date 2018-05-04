0be309720f593b31c0d7f959673ddd5ebbbb5975
edmondxiexie <xiejy36@gmail.com>
2018-05-03 21:23:57 -0700

    Add original dataset

--- src/components/app.js
+++ src/components/app.js
@@ -27,6 +27,8 @@ export default class App extends Component {
 
     const tripFiles = require.context(filePath, false, /\.json$/);
 
+    console.log("filePath", filePath);
+
     const data = [];
     const dataMap = {};
     let exampleTrip;
